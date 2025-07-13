6f997ae8124c1d8ac8c1dd48730d5463b79bbff2
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-07-14 06:58:21 +1000

    Update lsp.lua

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,3 +1,17 @@
+vim.lsp.enable({
+    "clangd",
+    "cssls",
+    "gopls",
+    "lua_ls",
+    "omnisharp",
+    "phpactor",
+    "prettier",
+    "pyright",
+    "sqlls",
+    "tailwindcss",
+    "ts_ls"
+})
+
 vim.diagnostic.config({
 	virtual_lines = false,
 	virtual_text = true,
