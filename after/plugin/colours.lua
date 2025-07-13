7fce55cd07732e86d0cd2475247f87c2700f4475
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-07-14 01:40:10 +1000

    Update colours.lua

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -2,8 +2,8 @@ function Colours(colour)
 	colour = colour -- or "poimandres"
 	vim.cmd.colorscheme(colour)
 
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 Colours()
