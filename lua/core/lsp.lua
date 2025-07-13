90940e79cc8eca76baf9639096f2ea8128cc7cc3
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-07-14 07:06:38 +1000

    Revert "Update lsp.lua"
    This reverts commit 6f997ae8124c1d8ac8c1dd48730d5463b79bbff2.

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,17 +1,3 @@
-vim.lsp.enable({
-    "clangd",
-    "cssls",
-    "gopls",
-    "lua_ls",
-    "omnisharp",
-    "phpactor",
-    "prettier",
-    "pyright",
-    "sqlls",
-    "tailwindcss",
-    "ts_ls"
-})
-
 vim.diagnostic.config({
 	virtual_lines = false,
 	virtual_text = true,
