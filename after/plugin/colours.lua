fc04cb3512efe0ac93484373972357a51f348d09
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-07-18 23:38:47 +1000

    .

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -4,6 +4,9 @@ function Colours(colour)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff", bold = true })
 end
 
 Colours()
