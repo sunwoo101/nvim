9d63ab586ab4200561b41c3452e8d0992056d03e
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-08-16 18:39:14 +1000

    Update lsp.lua

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,6 +1,6 @@
 vim.diagnostic.config({
     virtual_lines = false,
-    virtual_text = false,
+    virtual_text = true,
     severity_sort = true,
     underline = true,
     update_in_insert = true,
