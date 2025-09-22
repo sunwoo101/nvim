1a39e139601844aa5d2e502a002fb505fac20634
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-09-23 08:09:12 +1000

    Update treesitter.lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,4 @@
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
     ensure_installed = {
         "go",
         "python",
@@ -22,24 +21,24 @@ require 'nvim-treesitter.configs'.setup {
         "markdown",
         "markdown_inline"
     },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
     highlight = {
         enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enabled = true,
+    },
 }
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+    pattern = "python",
+    callback = function()
+        if pcall(function()
+                return require("nvim-treesitter.parsers").has_parser("python")
+            end) then
+            vim.bo.indentexpr = "nvim_treesitter#indent()"
+        end
+    end,
+})
