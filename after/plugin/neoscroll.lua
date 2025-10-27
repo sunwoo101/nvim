c1780bf4f498b871ab80159fd825eef28bb075ac
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-10-27 20:20:52 +1100

    Update neoscroll.lua

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -1,3 +1,4 @@
+--[[
 require('neoscroll').setup({
     mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>' },
     duration_multiplier = 0.5,
@@ -12,3 +13,11 @@ end, { silent = true })
 vim.keymap.set({ "n", "v" }, "<C-k>", function()
     ns.scroll(-vim.wo.scroll, { move_cursor = true, duration = 200 }) -- like <C-u>
 end, { silent = true })
+]]
+
+-- Instant scroll --
+-- half-page down
+vim.keymap.set({ "n", "v" }, "<C-j>", "<C-d>", { noremap = true, silent = true })
+
+-- half-page up
+vim.keymap.set({ "n", "v" }, "<C-k>", "<C-u>", { noremap = true, silent = true })
