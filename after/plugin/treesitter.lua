ba95246de69c3aa909690eaa29e918dd40c013d4
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2026-04-12 08:55:52 +1000

    fix lag and crashes

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -33,6 +33,7 @@ require 'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
+        disable = { "xml" },
     },
     indent = {
         enabled = false,
