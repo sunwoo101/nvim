6c8c15893fa6dc670dcbc58777ea9529eabc0f36
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2026-05-04 10:54:40 +1000

    neovide fixes for mac

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -9,13 +9,18 @@ if vim.g.neovide then
         vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
     end
 
-    map({ "n", "v" }, "<C-S-c>", '"+y', "Copy to clipboard")
-    map({ "n", "v" }, "<C-S-x>", '"+d', "Cut to clipboard")
-    map({ "n", "v", "s", "x", "o", "l", "c", "t" }, "<C-S-v>", function()
+    local is_mac = vim.fn.has("macunix") == 1
+    local copy_key   = is_mac and "<D-c>" or "<C-S-c>"
+    local cut_key    = is_mac and "<D-x>" or "<C-S-x>"
+    local paste_key  = is_mac and "<D-v>" or "<C-S-v>"
+
+    map({ "n", "v" }, copy_key, '"+y', "Copy to clipboard")
+    map({ "n", "v" }, cut_key, '"+d', "Cut to clipboard")
+    map({ "n", "v", "s", "x", "o", "l", "c", "t" }, paste_key, function()
         vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
     end, "Paste from clipboard")
-    map("i", "<C-S-v>", "<C-r>+", "Paste from clipboard")
-    map("c", "<C-S-v>", "<C-r>+", "Paste from clipboard")
+    map("i", paste_key, "<C-r>+", "Paste from clipboard")
+    map("c", paste_key, "<C-r>+", "Paste from clipboard")
 
     map("t", "<S-CR>", function()
         vim.api.nvim_chan_send(vim.bo.channel, "\27\r")
@@ -25,7 +30,10 @@ if vim.g.neovide then
     local function scale(delta)
         vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
     end
-    map("n", "<C-=>", function() scale(1.1) end, "Zoom in")
-    map("n", "<C-->", function() scale(1 / 1.1) end, "Zoom out")
-    map("n", "<C-0>", function() vim.g.neovide_scale_factor = 1.0 end, "Reset zoom")
+    local zoom_in  = is_mac and "<D-=>" or "<C-=>"
+    local zoom_out = is_mac and "<D-->" or "<C-->"
+    local zoom_rst = is_mac and "<D-0>" or "<C-0>"
+    map("n", zoom_in,  function() scale(1.1) end, "Zoom in")
+    map("n", zoom_out, function() scale(1 / 1.1) end, "Zoom out")
+    map("n", zoom_rst, function() vim.g.neovide_scale_factor = 1.0 end, "Reset zoom")
 end
